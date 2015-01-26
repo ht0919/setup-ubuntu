f6be89cb40333b0b051b410017b88d5ece4887fd
Hiroyuki Tanaka <ht0919@gmail.com>
2015-01-26 15:07:23 +0900

    add update-all.sh

---
+++ update-all.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+cd ~/.rbenv
+git pull
+cd ~/.rbenv/plugins/ruby-build
+git pull
+cd ~/.plenv
+git pull
+cd ~/.plenv/plugins/perl-build
+git pull
+cd ~/.pyenv
+git pull
+cd ~/.pyenv/plugins/pyenv-virtualenv
+git pull
+cd ~/.ndenv
+git pull
+cd ~/.ndenv/plugins/node-build
+git pull
+cd
+gem update --system
