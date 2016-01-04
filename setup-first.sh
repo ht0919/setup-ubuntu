918124e46cfdd8754eef9029fc6c4bb9a2f362e3
Hiroyuki Tanaka <ht0919@gmail.com>
2016-01-04 15:02:43 +0900

    Chg setup-first.sh

--- setup-first.sh
+++ setup-first.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install -y git git-core build-essential bison flex sqlite3  libsqlite3-dev autoconf libreadline6-dev zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt1-dev libncurses5-dev libreadline-dev libbz2-dev
+sudo apt-get install -y git git-core build-essential bison flex sqlite3  libsqlite3-dev autoconf libreadline6-dev zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt1-dev libncurses5-dev libreadline-dev libbz2-dev libffi-dev
