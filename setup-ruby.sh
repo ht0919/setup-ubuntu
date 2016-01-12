0902cfbaee0f4c77a05024ab2916d41a775ce8d8
Hiroyuki Tanaka <ht0919@gmail.com>
2016-01-12 14:07:47 +0900

    Chg ruby 2.2.4

--- setup-ruby.sh
+++ setup-ruby.sh
@@ -3,13 +3,11 @@ git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-bu
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bash_profile
 echo 'eval "$(rbenv init -)"' >> .bash_profile
 source .bash_profile
-rbenv install 2.3.0
+RUBY_CONFIGURE_OPTS="--disable-install-doc" rbenv install 2.2.4
 rbenv rehash
-rbenv global 2.3.0
+rbenv global 2.2.4
 gem update --system -N
 gem install bundler -N
-gem install rspec -N
-gem install therubyracer -N
 gem install rails -N
 rbenv rehash
 rails -v
