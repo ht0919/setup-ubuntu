c56d3224ca3fd071d683642ee17d848097e11439
Hiroyuki Tanaka <ht0919@gmail.com>
2016-03-25 12:34:34 +0900

    Chg Swift

--- setup-swift.sh
+++ setup-swift.sh
@@ -1,6 +1,6 @@
 sudo apt-get install clang libicu-dev
-wget https://swift.org/builds/ubuntu1404/swift-2.2-SNAPSHOT-2015-12-01-b/swift-2.2-SNAPSHOT-2015-12-01-b-ubuntu14.04.tar.gz
-tar xzf swift-2.2-SNAPSHOT-2015-12-01-b-ubuntu14.04.tar.gz
+wget https://swift.org/builds/swift-2.2-release/ubuntu1404/swift-2.2-RELEASE/swift-2.2-RELEASE-ubuntu14.04.tar.gz
+tar xzf swift-2.2-RELEASE-ubuntu14.04.tar.gz
 mkdir .swift
-mv ./swift-2.2-SNAPSHOT-2015-12-01-b-ubuntu14.04/usr ./.swift
+mv ./swift-2.2-RELEASE-ubuntu14.04/usr ./.swift
 echo 'export PATH=/home/vagrant/.swift/usr/bin:$PATH' >> .bash_profile
