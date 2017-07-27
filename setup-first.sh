4815a779af81bc4eef367278307af5b873c0b148
unknown <>
2017-07-27 17:03:56 +0900

    Change setup-first.sh setup-ruby.sh

--- setup-first.sh
+++ setup-first.sh
@@ -1,3 +1,1 @@
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get install -y git git-core build-essential bison flex sqlite3  libsqlite3-dev autoconf libreadline6-dev zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt1-dev libncurses5-dev libreadline-dev libbz2-dev libffi-dev
+sudo apt-get -y install git curl g++ make zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev sqlite3 libsqlite3-dev nodejs
--- setup-ruby.sh
+++ setup-ruby.sh
@@ -3,10 +3,11 @@ git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-bu
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bash_profile
 echo 'eval "$(rbenv init -)"' >> .bash_profile
 source .bash_profile
-rbenv install 2.3.0
+rbenv install 2.0.0-p353
+rbenv global 2.0.0-p353
 rbenv rehash
-rbenv global 2.3.0
 gem update --system -N
 gem install bundler -N
-gem install rails -N
+gem install rails -v 4.0.2 -N
+ruby -v
 rails -v
